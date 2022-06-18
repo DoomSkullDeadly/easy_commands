c3fd57a1996f1ea4957c1d9699a3f5b1d74224e0
DoomSkull <dfordoomskull@gmail.com>
2022-06-18 23:16:32 +0100

    Added block id debug rendering

--- Block.py
+++ Block.py
@@ -1,6 +1,8 @@
 import pygame
 import itertools
 
+pygame.init()
+
 size = 100, 60
 s = pygame.Surface(size=size)
 s_snap_bottom = pygame.Surface(size=(100, 20))
@@ -9,6 +11,11 @@ s_snap_top = pygame.Surface(size=(20, 20))
 s_snap_top.set_alpha(50)
 display = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
 counter = itertools.count()
+font = pygame.font.SysFont("Arial", 28, bold=True)
+
+
+def text_render(text):
+    return font.render(str(text), True, (0, 0, 0))
 
 
 class Block:
@@ -24,12 +31,14 @@ class Block:
         self.child = None
         self.parent = None
 
-    def render(self):
+    def render(self):  # TODO: rework to draw to surface once or when updated, only need to blit surface (efficiency)
         pygame.draw.rect(surface=s, color=(100, 130, 60), rect=s.get_rect())  # idk how to not use s.get_rect() :/
         if self.highlight:
             pygame.draw.rect(surface=s, color=(150, 255, 90), rect=s.get_rect(), width=2)
         else:
             pygame.draw.rect(surface=s, color=(50, 100, 30), rect=s.get_rect(), width=2)  # outline
+
+        s.blit(text_render(self.id), (s.get_rect().centerx-(6*len(str(self.id))), s.get_rect().centery-14))  # debug
         display.blit(s, self.rect)
 
         pygame.draw.rect(surface=s_snap_bottom, color=(200, 200, 200), rect=s_snap_bottom.get_rect())  # for debugging
